642c19fdc61bd076e5b9a2e7c6b68c2b3d010b1c
felixthiopp <124409045+felixthiopp@users.noreply.github.com>
2025-05-01 20:22:39 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/events', require('./routes/eventRoutes'));const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// DB + Start Server
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(5000, () => console.log('Server running on port 5000'));
+  })
+  .catch(err => console.log(err));
